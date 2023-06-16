dfe074c2ec4816bc5ac3d18a80c2175a2b5d519a
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-16 15:09:03 +0300

    creat model part 1  shared preferences

---
+++ lib/preferences/shared_pref_controller.dart
@@ -0,0 +1,37 @@
+import 'package:shared_preferences/shared_preferences.dart';
+enum PrefKey{theme,lauCode}
+class SharedPrefController{
+  SharedPrefController._();
+
+  late SharedPreferences _sharedPreferences;
+
+  static SharedPrefController? _instance;
+
+  factory SharedPrefController(){
+    return _instance??= SharedPrefController._();
+  }
+
+  Future<void> initSharedPref() async{
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  T? getValueFor<T>(String key) {
+    if(_sharedPreferences.containsKey(key)){
+      return _sharedPreferences.get(key) as T;
+    }
+    return null;
+  }
+
+  Future<bool> clear() async{
+    return _sharedPreferences.clear();
+  }
+
+  Future<bool> removeValueFor(String key) async{
+    if(_sharedPreferences.containsKey(key)){
+      return _sharedPreferences.remove(key) ;
+    }
+    return false;
+  }
+
+
+}
